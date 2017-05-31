50653cf9a4ef1fb17cd0f4cf257ca046e5de5804
ouxu <ouxu@outlook.com>
2017-05-31 12:06:10 +0800

    添加单位字段

--- controller/apply.js
+++ controller/apply.js
@@ -29,8 +29,11 @@ exports.submitSpecial = async (ctx, next) => {
   const createRule = {
     name: 'string',
     sex: 'string',
-    mobile: /(13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7}/ig,
-    email: /\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/
+    unit: {
+      require: false,
+      type: 'string'
+    },
+    mobile: /(13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7}/ig
   }
   try {
     ctx.verifyParams(createRule)
