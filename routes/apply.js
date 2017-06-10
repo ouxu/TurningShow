481dae24d6c576a940650c9355b95a734877bac9
ouxu <ouxu@outlook.com>
2017-06-10 21:35:05 +0800

    关闭报名

--- routes/apply.js
+++ routes/apply.js
@@ -1,8 +1,8 @@
 const router = require('koa-router')()
 const applyController = require('../controller/apply')
 
-router.post('/api/apply/submitTurning', applyController.submitTurning)
-router.post('/api/apply/submitTurningOnline', applyController.submitTurningOnline)
+// router.post('/api/apply/submitTurning', applyController.submitTurning)
+// router.post('/api/apply/submitTurningOnline', applyController.submitTurningOnline)
 router.post('/api/apply/submitSpecial', applyController.submitSpecial)
 router.post('/api/apply/submitInnovation', applyController.submitInnovation)
 router.get('/api/apply/download', applyController.download)
